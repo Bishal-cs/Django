6530bb73b48830a600efbb4e2a7c2284abfe8418
bishaldas1152006@gmail.com <bishaldas1152006@gmail.com>
2025-06-07 00:43:59 +0530

    learn a littlebit about database

--- StarterProject/practiceAPP/models.py
+++ StarterProject/practiceAPP/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class all_typs_imgs(models.Model):
+    TYPES_OF_CHOICES = [
+        ('type1', 'Type 1'),
+        ('type2', 'Type 2'),
+        ('type3', 'Type 3'),
+        ('type4', 'Type 4'),
+        ('type5', 'Type 5'),
+    ]
+    name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='images/')
+    date_time_stamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
--- StarterProject/practiceAPP/views.py
+++ StarterProject/practiceAPP/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import all_typs_imgs
 
 # Create your views here.
 def index(request):
-    return render(request, 'website/all_index.html')
+    imgs = all_typs_imgs.objects.all()
+    return render(request, 'website/all_index.html', {'imgs': imgs})
