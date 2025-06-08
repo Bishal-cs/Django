e97089fb4182509a9c40761f92f4d3c67789c2ad
bishaldas1152006@gmail.com <bishaldas1152006@gmail.com>
2025-06-08 22:51:33 +0530

    make the index page functional and cool

--- StarterProject/practiceAPP/admin.py
+++ StarterProject/practiceAPP/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import all_typs_imgs
+from .models import all_typs_imgs, Image_review, specific_image, ImageProfile
 
-# Register your models here.
-admin.site.register(all_typs_imgs)
+# Inline for Image_review in all_typs_imgs admin
+class ImageReviewInline(admin.TabularInline):
+    model = Image_review
+    extra = 2
+
+# Admin for all_typs_imgs
+class AllTypsImgsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_time_stamp')
+    inlines = [ImageReviewInline]
+
+# Admin for specific_image
+class SpecificImageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image_info')
+    filter_horizontal = ('image_type',)
+
+# Admin for ImageProfile
+class ImageProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'profile_number', 'isshu_date', 'valid_until')
+
+admin.site.register(all_typs_imgs, AllTypsImgsAdmin)
+admin.site.register(specific_image, SpecificImageAdmin)
+admin.site.register(ImageProfile, ImageProfileAdmin)
--- StarterProject/practiceAPP/models.py
+++ StarterProject/practiceAPP/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class all_typs_imgs(models.Model):
@@ -20,3 +21,41 @@ class all_typs_imgs(models.Model):
     
     def __str__(self):
         return self.name
+    
+# one to many relationship with user 
+class Image_review(models.Model):
+    image = models.ForeignKey(
+        all_typs_imgs,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    ratings = models.IntegerField(default=0),
+    comment = models.TextField(default=''),
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} reviewed {self.image.name}'
+    
+# Many to many relationship with user
+class specific_image(models.Model):
+    name = models.CharField(max_length=100)
+    image_info = models.CharField(max_length=100)
+    image_type = models.ManyToManyField(all_typs_imgs, related_name='specific_images')
+
+    def __str__(self):
+        return self.name
+    
+# one to one relationship with user
+
+class ImageProfile(models.Model):
+    user = models.OneToOneField(all_typs_imgs, on_delete=models.CASCADE, related_name='profile')
+    profile_number = models.CharField(max_length=100)
+    isshu_date = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.user.name} Profile'
